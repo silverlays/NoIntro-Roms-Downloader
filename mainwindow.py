a0b454dcd6e295ef2e046cebc95d8d6c597e7db8
Damien Lacroix <inforlac.lacroix@gmail.com>
2021-09-12 23:42:46 +0200

    v1.1 released

--- mainwindow.py
+++ mainwindow.py
@@ -1,10 +1,12 @@
+import asyncio
+import os
 import PySimpleGUI as sg
 import time
 import scrapper
 from threading import Thread
 
 
-__PROGRAM_VERSION__ = "1.0"
+__PROGRAM_VERSION__ = "1.1"
 __PROGRAM_TITLE__ = f"NoIntro Roms Downloader v{__PROGRAM_VERSION__}"
 
 APP_ICON_BASE64 = b"iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAIAAAD8GO2jAAAED0lEQVRIx61WvU5qSxSevz1s2BKwQkNhTKTkESwsLFQ6KUzUxGew8xF8AuNbGIilJRZGITGWaGJEiRYiDYTN/Jzig3Hc6Lnn3nNXocPsNev/W2tRxpgxhhDCObfWUkoJIbgRQhhjrLXGGCEE55wQQgjRWuOeUmqtJYTglbXWXTLGIIf6n3ELJv+Scz4ajYhHqVTKTIkQwhiDdEjDGdIganIlhIBQrTX5SsVisVKprKystNvtWq3W7XYTDIwxFwxjDOIxsRVCpZRxHLsHlNJUKrWxsVGtVkulUj6fX1paAs/j42O/32+1Wufn541Go9frzRoEsdT9Q3zX19ejKCqXy2tra6urq+TPyBhTq9Xq9frr62uz2ex2u5xzYwwcmoTIGCOlPD4+FkKoKWmt7ZRgCghpJ4QEQcAYo5QGQSCl5Jyfnp7e3t5yzuETpVQgMwji29sb0oBnsxnzma21SikYNxgMlFJBEOC5XzjEJTqTydi/psPDQ9QYwk4pFU6V1vry8nI0Gg2HwzAMs9kspVQp1ev1CCH5fD4IAq01Y6zf78dxHEVRFEWEkDiOPz4+tNaFQuH+/t4VutaaUkqcKmgql8vVanV5edl5WqlUtra2/KyWy+Xt7e1isZjgcVF1fxljBDhCFRUKhXa7ba09OTlJpVKc84ODA/i+t7cnhAiCYGFh4eHhwVpbr9fT6bQQYn9/Hzy7u7uUUiEEjIZM4VeIlNIYE8fx4uIigpbJZACOXC6nlEL/GI/HcRxLKQkhSqlcLgeefD5vrUUJudoTfpHANSnl3NwctGqtIWg8HrsaZYxJKRFlfPJ5XM/AWfhX7gMA4t/4OAAIEpfuJ766hyIBS1/5T/c4o6E63LnOmiCRMNxZ9y13wt7PhjOl7xXMuvkb6ZTSBKqdHz964DS7DjHL6lvnDIcmv6/MJkYklH/rZiIZfuhRab+JqnC2+4nC9PD1uQNgP0Hp9CZx8EMtMHpghUuyO+CNP+OMMTg7e90nINFFbzLYUW2A32ygx+MxRoez10HEHfB2tsNP5oEfryAI0um0P2PDMIRoKSUOYRiGYUgISafT4AmCAI6iS8P8L63Chb7T6VxdXUVRdHFxMRwOCSHNZrPT6Vhrb25u4PLT0xN4Go3GYDAghLRarefnZ2vt9fU1Mu8m5WToM8a01pzzUqmEqYR5CSZkNY5jhBU7EloeVKJ9YcC9vLz0ej1/6/kEZDab/fuJdnR0BH0OH18Wr83NTRRVYv36CRA+KrELNRqN9/d3B0CttUAZKKXOzs4KhYJSChvjP+LO3+Ncgc3Pz+/s7Nzd3QkhMBWEj/hiseiK9c89cOSWDB+wn4sf+f/IQX2yVUChW2n+VX/1QwROpZTvHE2g10X/vxnuZx5R+QW5F6NjoSC40gAAAABJRU5ErkJggg=="
@@ -37,8 +39,21 @@ class MainWindow():
   def __init__(self):
       sg.theme("DarkGrey14")
       sg.set_global_icon(APP_ICON_BASE64)
-      self.pb_fetch_value = 0
-      self.pb_download_value = 0
+
+      self.pb_fetch  = {
+        "value": 0,
+        "max": 0
+      }
+      self.pb_download = {
+        "value": 0,
+        "max": 0
+      }
+      self.selected_infos = {
+        "platform": "",
+        "games_count": int(0),
+        "game": "",
+        "game_size": float(0),
+      }
 
 
   def show(self):
@@ -53,7 +68,6 @@ class MainWindow():
       if event == "combo_platform": self._combo_platform_changed(values['combo_platform'])
       if (event == "search_button" or event == "input_search_game::RETURN") and self.window['input_search_game'] != "": self._search_button_clicked(values['input_search_game'])
       if event == "select_button": self._select_button_clicked(values['combo_select_game'])
-      if event == "output_folder": self._switch_disabled_frame("frame5", False)
       if event == "download_button": self._download_button_clicked(values['output_folder'])
 
 
@@ -70,11 +84,11 @@ class MainWindow():
 
     frame3_layout = [
       [sg.Combo([], key="combo_select_game", readonly=True, expand_x=True), sg.Button("Select", key="select_button")],
-      [sg.Text("Selected game:", pad=((5, 0), 5)), sg.Text("n/a", key="text_select_game", pad=((0, 5), 5))]
+      [sg.Text("Selected game:", pad=((5, 0), 5)), sg.Text("n/a", key="text_select_game", pad=((0, 5), 5)), sg.Text("", expand_x=True), sg.Text("Selected game size:", pad=((5, 0), 5)), sg.Text("n/a", key="text_select_game_size", pad=((0, 5), 5))]
     ]
 
     frame4_layout = [
-      [sg.Input(key="output_folder", expand_x=True, enable_events=True), sg.FolderBrowse(key="browse_button")]
+      [sg.Input(default_text=os.getcwd(), key="output_folder", expand_x=True, enable_events=True), sg.FolderBrowse(key="browse_button", initial_folder=os.getcwd())]
     ]
 
     frame5_layout = [
@@ -82,12 +96,12 @@ class MainWindow():
     ]
 
     layout = [
-      [sg.Frame("1. Select the platform", frame1_layout, key="frame1", expand_x=True)],
-      [sg.Frame("2. Search game(s) by keywords", frame2_layout, key="frame2", expand_x=True)],
-      [sg.Frame("3. Select game", frame3_layout, key="frame3", expand_x=True)],
-      [sg.Frame("4. Select output folder", frame4_layout, key="frame4", expand_x=True)],
-      [sg.Frame("5. Download game", frame5_layout, key="frame5", expand_x=True)],
-      [sg.ProgressBar(10, key="progress_bar_download", size=(0, 15), expand_x=True)]
+      [sg.Frame("1. Select the platform", frame1_layout, key="frame1", expand_x=True, pad=(5, 10))],
+      [sg.Frame("2. Search game(s) by keywords", frame2_layout, key="frame2", expand_x=True, pad=(5, 10))],
+      [sg.Frame("3. Select game", frame3_layout, key="frame3", expand_x=True, pad=(5, 10))],
+      [sg.Frame("4. Select output folder", frame4_layout, key="frame4", expand_x=True, pad=(5, 10))],
+      [sg.Frame("5. Download game", frame5_layout, key="frame5", expand_x=True, pad=(5, 10))],
+      [sg.ProgressBar(10, key="progress_bar_download", size=(0, 15), expand_x=True), sg.Text("0Kb / 0Kb", key="text_download_status")]
     ]
 
     return sg.Window(__PROGRAM_TITLE__, layout, finalize=True)
@@ -95,45 +109,43 @@ class MainWindow():
 
   def _combo_platform_changed(self, value: str):
     self._reset_values()
-    platform_id = PLATFORM_IDENTIFIERS[value]
-    self.window['text_platform'].update(platform_id)
-    thread = Thread(target=scrapper.download_platform_data, args=(platform_id,), daemon=True)
+    self.selected_infos['platform'] = PLATFORM_IDENTIFIERS[value]
+    self.window['text_platform'].update(self.selected_infos['platform'])
+    self.window["progress_bar_fetch"].update(0, 0)
+    thread = Thread(target=scrapper.download_platform_data, args=(self.selected_infos['platform'], self.window['progress_bar_fetch'], self._combo_platform_changed_callback))
     thread.start()
-    while thread.is_alive():
-      self._update_progress_bar(self.window['progress_bar_fetch'], self.pb_fetch_value)
-      time.sleep(0.5)
-    self._update_progress_bar(self.window['progress_bar_fetch'], self.pb_fetch_value, finished=True)
+
+
+  def _combo_platform_changed_callback(self):
     self.window['text_games_count'].update(len(scrapper.platform_games_buffer))
     self._switch_disabled_frame("frame2", False)
 
 
   def _search_button_clicked(self, value: str):
-    games_count = scrapper.search_game(value)
-    self.window['text_search_game'].update(games_count)
+    self.selected_infos['games_count'] = scrapper.search_game(value)
+    self.window['text_search_game'].update(self.selected_infos['games_count'])
     self._switch_disabled_frame("frame3", False)
     self.window['combo_select_game'].update(values=[game for game in scrapper.games_found])
 
 
   def _select_button_clicked(self, value: str):
-    self.window['text_select_game'].update(value)
+    self.selected_infos['game'] = value
+    self.selected_infos['game_size'] = scrapper.get_file_length(self.selected_infos['platform'], self.selected_infos['game'])
+    self.window['text_select_game'].update(self.selected_infos['game'])
+    self.window['text_select_game_size'].update(scrapper.length_to_unit_string(self.selected_infos['game_size']))
+    self.window['progress_bar_download'].update(0, max=self.selected_infos['game_size'])
     self._switch_disabled_frame("frame4", False)
+    self._switch_disabled_frame("frame5", False)
 
 
   def _download_button_clicked(self, value: str):
-    thread = Thread(target=scrapper.download_file, args=(self.window['text_platform'].DisplayText, self.window['text_select_game'].DisplayText, value,))
+    thread = Thread(target=scrapper.download_file, args=(self.selected_infos['platform'], self.selected_infos['game'], value, self.window['progress_bar_download'], self.window['text_download_status'], self._download_button_clicked_callback))
     thread.start()
-    while thread.is_alive():
-      self._update_progress_bar(self.window['progress_bar_download'], self.pb_download_value)
-      time.sleep(0.5)
-    self._update_progress_bar(self.window['progress_bar_download'], self.pb_download_value, finished=True)
 
 
-  def _update_progress_bar(self, pb: sg.ProgressBar, current_value: int, finished=False):
-    if finished: current_value = pb.MaxValue
-    else:
-      if current_value < pb.MaxValue: current_value+=1
-      else: current_value = 0
-    pb.update(current_value)
+  def _download_button_clicked_callback(self):
+    print("FINISH")
+    #sg.popup_quick("FINISH")
 
 
   def _reset_values(self):
@@ -145,8 +157,10 @@ class MainWindow():
     self.window['text_search_game'].update("n/a")
     self.window['combo_select_game'].update([])
     self.window['text_select_game'].update("n/a")
+    self.window['text_select_game_size'].update("n/a")
+    self.window['output_folder'].update(os.getcwd())
     self.window['progress_bar_download'].update(0)
-    self.window['output_folder'].update("")
+    self.window['text_download_status'].update("0Kb / 0Kb")
 
 
   def _switch_disabled_frame(self, frame: str, make_disabled: bool):
--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,12 @@
+import asyncio
+from asyncio.streams import StreamReader, StreamWriter
+from http.client import HTTPResponse
+import PySimpleGUI as sg
 import os
-from urllib.parse import quote
+import math
 from urllib.request import urlopen, urlretrieve
+from urllib.parse import quote
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 from bs4.element import SoupStrainer, Tag
 
@@ -10,16 +16,28 @@ platform_games_buffer = []
 games_found = []
 
 
-def download_platform_data(platform_id: str) -> int:
+def download_platform_data(platform_id: str, cb_progressbar: sg.ProgressBar, cb_function):
   global platform_games_buffer
-  platform_games_buffer, buffer_temp = ([], [])
+  dom: BeautifulSoup = None
+  internaL_count = 0
+  internal_max = 100
+
+  def update():
+    nonlocal internaL_count
+    internaL_count += 33
+    cb_progressbar.update(internaL_count, internal_max)
+
   with urlopen(f"{_base_url}/{platform_id}") as response:
+    update()
     dom = BeautifulSoup(response, features="html.parser", parse_only=SoupStrainer("a"))
+    update()
+
   for query in dom.contents:
     query: Tag
-    if query.text.find(".7z") != -1:
-      buffer_temp.append(query.text)
-  platform_games_buffer = buffer_temp
+    if query.text.find(".7z") != -1: platform_games_buffer.append(query.text)
+
+  update()
+  cb_function()
 
 
 def search_game(keywords: str) -> int:
@@ -43,6 +61,54 @@ def search_game(keywords: str) -> int:
   return len(games_found)
 
 
-def download_file(patform_id: str, filename: str, output_folder: str):
+def download_file(platform_id: str, filename: str, output_folder: str, cb_progressbar: sg.ProgressBar, cb_text_status: sg.Text, cb_function):
   filename_quoted = quote(filename)
-  urlretrieve(url=f"{_base_url}/{patform_id}/{filename_quoted}", filename=os.path.join(output_folder, filename))
+  full_url = f"{_base_url}/{platform_id}/{filename_quoted}"
+  output_stream = open(file=os.path.join(output_folder, filename), mode="wb")
+  file_request: HTTPResponse = urlopen(full_url)
+  file_length = file_request.length
+  buffer_length = int(file_length / 10)
+  position = 0
+  
+  while True:
+    bytes_read = file_request.read(buffer_length)
+    output_stream.write(bytes_read)
+    output_stream.flush()
+    position += len(bytes_read)
+    cb_progressbar.update(position, file_length)
+    cb_text_status.update(f"{length_to_unit_string(position)} / {length_to_unit_string(file_length)}")
+    if len(bytes_read) < buffer_length: break
+  
+  file_request.close()
+  output_stream.close()
+  while not file_request.closed and not output_stream.closed: pass
+
+  cb_function()
+
+
+def get_file_length(platform_id: str, filename: str) -> float:
+  filename_quoted = quote(filename)
+  full_url = f"{_base_url}/{platform_id}/{filename_quoted}"
+
+  try:
+    with urlopen(url=full_url, timeout=2) as response: return float(response.length)
+  except HTTPError: return float(0)
+
+
+def length_to_unit_string(length: float) -> str:
+  unit = "b"
+  one_kilobytes = math.pow(1024, 1)
+  one_megabytes = math.pow(1024, 2)
+  one_gigabytes = math.pow(1024, 3)
+
+  if length > one_kilobytes and length < one_megabytes:
+    length /= one_kilobytes
+    unit = "Kb"
+  if length > one_megabytes and length < one_gigabytes:
+    length /= one_megabytes
+    unit = "Mb"
+  if length > one_gigabytes:
+    length /= one_gigabytes
+    unit = "Gb"
+  
+  return "{:.1f}{}".format(length, unit)
