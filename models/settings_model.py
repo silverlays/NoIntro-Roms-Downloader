c82c7c672f0ed0eca9dcaf2e44738141bc22f9f9
Damien Lacroix <inforlac.lacroix@gmail.com>
2026-02-05 03:39:02 +0100

    First stage

--- models/settings_model.py
+++ models/settings_model.py
@@ -1,14 +1,48 @@
-from dataclasses import dataclass, asdict
 from pathlib import Path
 
-
-@dataclass
-class _SettingsData:
-    first_run: bool = True
-    download_folder: Path | None = None
+from PySide6.QtCore import QSettings
 
 
-class SettingsModel:
+class _UserSettings:
+    def __init__(self, first_run: bool = True, download_folder: str = ".") -> None:
+        self.FirstRun = first_run
+        self.DownloadFolder = download_folder
+
+    @property
+    def FirstRun(self):
+        return self._first_run
+
+    @FirstRun.setter
+    def FirstRun(self, value: bool):
+        self._first_run = value
+
+    @property
+    def DownloadFolder(self):
+        return self._download_folder.absolute().__str__()
+
+    @DownloadFolder.setter
+    def DownloadFolder(self, value: str):
+        if Path(value).exists():
+            self._download_folder = Path(value)
+        else:
+            self._download_folder = Path(".")
+
+
+class SettingsModel(QSettings):
     def __init__(self):
         # TODO Write settings code.
-        pass
+        super().__init__()
+        self.user_settings = _UserSettings()
+        self.loadSettings()
+
+    def loadSettings(self):
+        self.user_settings.FirstRun = self.value(
+            "first_run", self.user_settings.FirstRun, bool
+        )  # type: ignore
+        self.user_settings.DownloadFolder = self.value(
+            "download_folder", self.user_settings.DownloadFolder, str
+        )  # type: ignore
+
+    def saveSettings(self):
+        self.setValue("first_run", self.user_settings.FirstRun)
+        self.setValue("download_folder", self.user_settings.DownloadFolder)
